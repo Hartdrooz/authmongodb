3fcfd360cb2670857833dfd2d197ea957f75251f
Hugo Girard <hugogirard@AO-watch.bup.local>
2018-06-23 15:55:11 -0400

    done

--- backend/routes/account.route.js
+++ backend/routes/account.route.js
@@ -1,15 +1,23 @@
 
 
 class AccountRoute {
-    constructor(express) {
+    constructor(express, mongoRepository) {
         this.express = express;
     }
 
     init() {
         const router = this.express.Router();
 
-        router.post('/register', () => {
+        router.post('/register', (req, res) => {
+            const username = req.body.username;
+            const password = req.body.password;
 
+            const user = new this.mongoRepository.User({
+                username: username,
+                password: password
+            });
+
+            user.save();
         });
 
         return router;
